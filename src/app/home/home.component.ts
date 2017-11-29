e197765d8a451433d4d8ff591e4327c8d3def54e
Remi Kristelijn <rkristelijn@gmail.com>
2017-11-29 17:47:23 +0000

    added ngModel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public languages: string[] = ["English", "Spanish", "Dutch", "Other"]; 
+  model = new Employee('Darla', 'Smith');
   constructor() { }
 
   ngOnInit() {
